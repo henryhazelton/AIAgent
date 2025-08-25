6ae620a52d7ab3948a803c7630f9181bdfbec248
henryhazelton <henryhazelton00@gmail.com>
2025-08-25 14:06:58 +0100

    updated main funciton

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
-
+def generate_content(client, messages):
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} Response tokens: {response.usage_metadata.candidates_token_count}")
 
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
     if len(sys.argv) < 2:
         print("Must input a prompt for the program to run")
         sys.exit(1)
     else:
-        prompt = sys.argv[1]
-    response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=prompt
-    )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} Response tokens: {response.usage_metadata.candidates_token_count}")
+        user_prompt = sys.argv[1]
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+
+    client = genai.Client(api_key=api_key)
+
+    generate_content(client, messages)
 
 
 
