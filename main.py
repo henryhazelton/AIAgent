4b5e4a7fd61385ab9a3c10f1d8525ccb674cf1bb
henryhazelton <henryhazelton00@gmail.com>
2025-08-30 22:15:27 +0100

    added a --verbose flag to command line argument

--- main.py
+++ main.py
@@ -8,25 +8,41 @@ def generate_content(client, messages):
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=messages
     )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} Response tokens: {response.usage_metadata.candidates_token_count}")
+    generated_text, prompt_tokens, response_tokens = response.text, f"Prompt tokens: {response.usage_metadata.prompt_token_count}", f"Response tokens: {response.usage_metadata.candidates_token_count}"
+    # prompt_tokens = f"Prompt tokens: {response.usage_metadata.prompt_token_count}"
+    # response_tokens = f"Response tokens: {response.usage_metadata.candidates_token_count}"
+    return generated_text, prompt_tokens, response_tokens
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
-    if len(sys.argv) < 2:
-        print("Must input a prompt for the program to run")
-        sys.exit(1)
-    else:
-        user_prompt = sys.argv[1]
-        messages = [
+    user_prompt = sys.argv[1]
+    messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
         ]
 
     client = genai.Client(api_key=api_key)
 
-    generate_content(client, messages)
+    generated_text, prompt_tokens, response_tokens  = generate_content(client, messages)
+
+    if len(sys.argv) < 2:
+        print("Must input a prompt for the program to run")
+        sys.exit(1)
+    elif len(sys.argv) > 2 and sys.argv[2] == '--verbose':
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print(generated_text)
+        
+        
+    
+
+
+    
+
+    
 
 
 
