9b34dc2cec5802372ac3300cb7ec19263d4caf36
henryhazelton <henryhazelton00@gmail.com>
2025-10-18 16:22:10 +0100

    updated tests.py

--- tests.py
+++ tests.py
@@ -3,34 +3,30 @@ from functions.get_file_content import get_file_content
 
 
 if __name__ == "__main__":
-    # # 1) current directory
-    # print("Result for current directory:")
-    # result = get_files_info("calculator", ".")
-    # # print each line, indented by one space
-    # for line in result.splitlines():
-    #     print(f" {line}")
+    # 1) current directory
+    print("Result for current directory:")
+    result = get_file_content("calculator", "main.py")
+    # print each line, indented by one space
+    for line in result.splitlines():
+        print(f" {line}")
 
-    # # 2) pkg directory
-    # print("Result for 'pkg' directory:")
-    # result = get_files_info("calculator", "pkg")
+    # 2) pkg directory
+    print("Result for 'pkg' directory:")
+    result = get_file_content("calculator", "pkg/calculator.py")
 
-    # for line in result.splitlines():
-    #     print(f" {line}")
+    for line in result.splitlines():
+        print(f" {line}")
 
-    # # 3) bin directory
-    # print("Result for '/bin' directory:")
-    # result = get_files_info("calculator", "/bin")
+    # 3) bin directory
+    print("Result for '/bin' directory:")
+    result = get_file_content("calculator", "/bin/cat")
 
-    # for line in result.splitlines():
-    #     print(f" {line}")
+    for line in result.splitlines():
+        print(f" {line}")
 
-    # # 4) ../ directory
-    # print("Result for '../' directory:")
-    # result = get_files_info("calculator", "../")
+    # 4) ../ directory
+    print("Result for '../' directory:")
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
 
-    # for line in result.splitlines():
-    #     print(f" {line}")
-
-    result = get_file_content("calculator", "lorem.txt")
     for line in result.splitlines():
         print(f" {line}")
